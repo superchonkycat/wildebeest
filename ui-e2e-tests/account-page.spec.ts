c85bab681a41f6bc2c3a8d847fa3ff3f65c8f59a
Dario Piotrowicz <dario@cloudflare.com>
2023-02-10 16:21:01 +0000

    Adjust account urls
    second attempt after fist caused issues and had
    to be reverted

--- ui-e2e-tests/account-page.spec.ts
+++ ui-e2e-tests/account-page.spec.ts
@@ -1,24 +1,31 @@
 import { test, expect } from '@playwright/test'
 
-test('Navigation to and view of an account (with 1 post)', async ({ page }) => {
-	await page.goto('http://127.0.0.1:8788/explore')
-	await page.getByRole('article').getByRole('link', { name: 'Ben Rosengart', exact: true }).click()
-	await page.getByRole('link', { name: 'Ben Rosengart', exact: true }).click()
-	await page.waitForLoadState('networkidle')
-	await page.getByRole('link', { name: 'Ben Rosengart', exact: true }).click()
+const navigationsVia = ['name link', 'avatar'] as const
 
-	await expect(page.getByTestId('account-info').getByRole('img', { name: 'Header of Ben Rosengart' })).toBeVisible()
-	await expect(page.getByTestId('account-info').getByRole('img', { name: 'Avatar of Ben Rosengart' })).toBeVisible()
-	await expect(page.getByTestId('account-info').getByRole('heading', { name: 'Ben Rosengart' })).toBeVisible()
-	await expect(page.getByTestId('account-info').getByText('Joined')).toBeVisible()
-	await expect(page.getByTestId('account-info').getByTestId('stats')).toHaveText('1Posts0Following0Followers')
+navigationsVia.forEach((via) =>
+	test(`Navigation via ${via} to and view of an account (with 1 post)`, async ({ page }) => {
+		await page.goto('http://127.0.0.1:8788/explore')
+		await page.getByRole('article').getByRole('link', { name: 'Ben Rosengart', exact: true }).click()
+		await page.waitForLoadState('networkidle')
+		if (via === 'name link') {
+			await page.getByRole('link', { name: 'Ben Rosengart', exact: true }).click()
+		} else {
+			await page.getByRole('link', { name: 'Avatar of Ben Rosengart' }).click()
+		}
 
-	expect(await page.getByTestId('account-statuses').getByRole('article').count()).toBe(1)
-	await expect(
-		page.getByTestId('account-statuses').getByRole('article').getByRole('img', { name: 'Avatar of Ben Rosengart' })
-	).toBeVisible()
-	await expect(page.getByTestId('account-statuses').getByRole('article')).toContainText('What fresh hell is this?')
-})
+		await expect(page.getByTestId('account-info').getByRole('img', { name: 'Header of Ben Rosengart' })).toBeVisible()
+		await expect(page.getByTestId('account-info').getByRole('img', { name: 'Avatar of Ben Rosengart' })).toBeVisible()
+		await expect(page.getByTestId('account-info').getByRole('heading', { name: 'Ben Rosengart' })).toBeVisible()
+		await expect(page.getByTestId('account-info').getByText('Joined')).toBeVisible()
+		await expect(page.getByTestId('account-info').getByTestId('stats')).toHaveText('1Posts0Following0Followers')
+
+		expect(await page.getByTestId('account-statuses').getByRole('article').count()).toBe(1)
+		await expect(
+			page.getByTestId('account-statuses').getByRole('article').getByRole('img', { name: 'Avatar of Ben Rosengart' })
+		).toBeVisible()
+		await expect(page.getByTestId('account-statuses').getByRole('article')).toContainText('What fresh hell is this?')
+	})
+)
 
 test('Navigation to and view of an account (with 2 posts)', async ({ page }) => {
 	await page.goto('http://127.0.0.1:8788/explore')
